6be08d64b7438659eaa2ddc204a20218f5d8cc17
Abhishek (Desktop) <1171abhishek.com>
2024-07-30 19:58:09 +0530

    complete naming rules

---
+++ src/topic_01_variable/04_naming_rules.kt
@@ -0,0 +1,22 @@
+package topic_01_variable
+
+/** Naming Rules
+ * Case sensitive
+ * name only contain letters , underscore and digits
+ * name cannot start with digit
+ * name cannot be a keyword
+ * wide space are not allowed, but we can use `` to contain wide space
+ * use camelCase for variable naming
+ * for constant use snake_case and all letters should be upper case only
+ */
+
+const val WEEK_DAYS = 7
+fun main() {
+    val `my name` = "Abhishek" // we should not use this
+    println(`my name`)
+
+    val myNameIs = "Abhishek"  // camelCase
+    println(myNameIs)
+
+    println(WEEK_DAYS)
+}
---
+++ src/topic_03_standard_output/01_statndard_output.kt
@@ -0,0 +1,5 @@
+package topic_03_standard_output
+
+fun main() {
+    // , the standard output displays the data on the screen
+}
