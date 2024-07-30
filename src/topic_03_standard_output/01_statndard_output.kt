047fc09c4402666b7ea4da6008629fd5dc5ef5f0
Abhishek (Laptop) <1171abhishek@gmail.com>
2024-07-30 20:22:31 +0530

    Adding code

--- src/topic_03_standard_output/01_statndard_output.kt
+++ src/topic_03_standard_output/01_statndard_output.kt
@@ -1,5 +1,31 @@
 package topic_03_standard_output
 
 fun main() {
-    // , the standard output displays the data on the screen
+    // standard output displays the data on the screen
+
+    // println() print with new line
+    println("Hello")
+    println("I")
+    println("am")
+    println("Kotlin")
+
+    println("Kotlin is a modern programming language.")
+    println() // prints an empty line
+    println("It is used all over the world!")
+
+    // print()  without new line 
+    print("hello")
+    print(" World")
+
+    // String templates 
+
+    val name = "Abhishek"
+    println("Hello I am $name")
+
+    val num1 = 12
+    val num2 = 34
+    println("The sum of $num1 and $num2 is ${num1 + num2}")
+
+
+
 }
