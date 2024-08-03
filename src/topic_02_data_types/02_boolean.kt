1f7a2e2ea677a40296171fe0fad8ddc819e0ccea
Abhishek (Desktop) <1171abhishek.com>
2024-08-03 13:46:18 +0530

    boolean half code added

---
+++ src/topic_02_data_types/02_boolean.kt
@@ -0,0 +1,14 @@
+package topic_02_data_types
+
+fun main() {
+
+    val yes:Boolean = true
+    val no:Boolean = false
+    println(yes)
+    println(no)
+
+    // user input boolean
+    val b:Boolean = readln().toBoolean()
+    println(b)
+
+}
