7ba18f66440cdcbe1a5df6b1824d9c55fbbf673a
Abhishek (Desktop) <1171abhishek.com>
2024-09-01 07:09:15 +0530

    pata nhi

--- src/topic_00_practise/main.kt
+++ src/topic_00_practise/main.kt
@@ -1,11 +1,27 @@
 package topic_00_practise
 
-fun main2() {
-    val pi = 3.1423
-    println(pi)
-    val piOneDigit = ("%.1f".format(pi)).toFloat()
-    println(piOneDigit)
-}
-fun main(){
-    println("hello\n".repeat(4))
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+fun main() {
+
+
+
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("hh:mm:ss a")
+        val formatted = current.format(formatter)
+
+
+    val hour = current.format(DateTimeFormatter.ofPattern("hh"))
+    val minute = current.format(DateTimeFormatter.ofPattern("mm"))
+    val second = current.format(DateTimeFormatter.ofPattern("ss"))
+    val amPm = current.format(DateTimeFormatter.ofPattern("a"))
+
+    println("Hour: $hour")
+    println("Minute: $minute")
+    println("Second: $second")
+    println("AM/PM: $amPm")
+
+        println("Current time: $formatted")
+
+
 }
--- src/topic_06_operators/01_increment_decrement.kt
+++ src/topic_06_operators/01_increment_decrement.kt
@@ -1,5 +1,20 @@
 package topic_06_operators
 
 fun main() {
+    // postfix form -> Change the value of a variable after it is used.
+    var number = 1
+    number++
+    println(number)
+    number--
+    println(number)
+
+     // postfix form -> Change the value of a variable before it is used.
+    var num = 1
+    ++num
+    println(num)
+    --num
+    println(num)
+
+
 
 }
