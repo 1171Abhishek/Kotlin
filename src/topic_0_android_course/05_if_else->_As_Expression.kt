a43d4dc8f44e003cd3c25ee1af4ede9624a94885
Abhishek(Laptop) <1171abhishek@gmail.com>
2024-10-16 10:59:36 +0530

    conditional code added

---
+++ src/topic_0_android_course/05_if_else->_As_Expression.kt
@@ -0,0 +1,15 @@
+package topic_0_android_course
+
+fun main() {
+//    If else and else if as expression
+    val number = 12
+    val ifElseAndElseIfExpression =
+        if (number == 1) "Twelve"
+        else if (number == 2) "Two"
+        else "Three"
+    println(ifElseAndElseIfExpression)
+
+//    Shorthand if else
+    val shorthandTry = if (1 == 2) "False" else "True"
+    println(shorthandTry)
+}
