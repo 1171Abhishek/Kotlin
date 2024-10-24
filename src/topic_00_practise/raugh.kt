112071b21affe08305a57a43069920bb093e3b24
Abhishek(Laptop) <1171abhishek@gmail.com>
2024-10-24 17:39:51 +0530

    Raugh code added

--- src/topic_00_practise/raugh.kt
+++ src/topic_00_practise/raugh.kt
@@ -1,14 +1,35 @@
 package topic_00_practise
+import java.util.Timer
+import kotlin.concurrent.schedule
+import kotlin.random.Random
 
 fun main() {
-    val name = null
-    println(name)
-    var name2: String? = "Abhishek"
-    println(name2)
-    println(name2?.length)
-    name2 = null
-    println(name2)
-    println(name2?.length?:10)
+    val randomNumber = Random.nextFloat()*2-1
+    println(randomNumber)
+    val niftyValue = 24_000
+    println(String.format("%.2f",randomNumber))
+//    val currentNiftyValue =
 
 
-}
+
+        val timer = Timer()
+
+        // Schedule a task to run every second
+        timer.schedule(0, 1000) {
+            // Generate and refresh random values here
+            val randomValue = (Math.random() * 100).toFloat() // Random float between 0.0 and 100.0
+            println("Random Value: ${"%.2f".format(randomValue)}")
+            if (randomValue > 0.0){
+                println(niftyValue+randomValue)
+            } else{
+                println(niftyValue - randomValue)
+            }
+            println(randomNumber)
+        }
+
+        // Keep the main thread alive to see the output (optional)
+        Thread.sleep(10000) // Run for 10 seconds before stopping
+        timer.cancel() // Stop the timer
+    }
+
+
