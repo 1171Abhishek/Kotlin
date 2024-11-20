7590a1299c355fedd03fdf3cf66d8380214fe1e7
1171abhishek <1171abhishek@gmail.com>
2024-11-20 13:04:56 +0000

    ok

---
+++ src/topic_00_practise/ok.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Enter you name")
+    var name = readln()
+    println("Enter your age")
+    var age = readln().toInt()
+    println("Your name is $name and your are $age years old")
+
+
+
+    
+}
