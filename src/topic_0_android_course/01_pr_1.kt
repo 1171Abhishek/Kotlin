20c74e2d05023c2e6fbb5923b90a19e64a35a65f
Abhishek(Laptop) <1171abhishek@gmail.com>
2025-01-15 21:31:22 +0530

    gh code added

--- src/topic_0_android_course/01_pr_1.kt
+++ src/topic_0_android_course/01_pr_1.kt
@@ -1,27 +1,5 @@
 package topic_0_android_course
 
-// This is Rough part for my practise
-
-class Practise1 {
-    // class property
-    val name: String = "Abhishek kumar"
-
-    //    class method
-    fun printName() {
-        println("Hello this is function 1")
-    }
-}
-
-
 fun main() {
-//    object created
-    val obj1 = Practise1()
 
-    println(obj1.name) // property access
-    obj1.printName() // access method of class
-}
-
-
-fun addTwoNumbersWithGemini(a: Int, b: Int): Int {
-    return a + b
 }
--- src/topic_0_android_course/07_OOPS_setter_getter.kt
+++ src/topic_0_android_course/07_OOPS_setter_getter.kt
@@ -1,13 +1,24 @@
 package topic_0_android_course
 
-class SetterAndGetterClass{
-    val name = "Setter and Getter Class"
+//In Kotlin, getters and setters are methods used to access (get) and modify (set) the properties of a class.
+
+class SetterGetterClass {
+    var age = 10
+
+        //    getter
+        get() = field
+
+        //    Setter
+        set(value) {
+            field = if (value < 18){
+                18
+            } else{
+                20
+            }
+        }
 }
 
-fun main(){
-    println("Setter and Getter")
-    val obj1 = SetterAndGetterClass()
-    println(obj1.name)
-    
+fun main() {
+     val obj1 = SetterGetterClass()
+    println(obj1.age)
 }
-
