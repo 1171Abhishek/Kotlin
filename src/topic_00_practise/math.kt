b0829d4a192b6502b6b4650fcc23d024aa261d71
Abhishek(Laptop) <1171abhishek@gmail.com>
2025-01-25 19:34:52 +0530

    Added practise code for math operations

---
+++ src/topic_00_practise/math.kt
@@ -0,0 +1,18 @@
+package topic_00_practise
+import kotlin.math.pow
+
+fun main() {
+    // do not change this code
+    val number = readln().toDouble()
+
+    // write your code below
+    val sampleInput1 = Math.sqrt(number)
+    println(sampleInput1)
+
+    val result = 6.0.pow(2.0)
+    println(result)
+
+    var result2 = 12.0.coerceAtLeast(65.5)
+
+
+}
