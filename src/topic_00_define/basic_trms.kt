e747e834924cc016464726106a68a97cead27eac
Abhishek(Laptop) <1171abhishek@gmail.com>
2025-02-24 11:23:09 +0530

    docs: Added basic terms definition for topic 00

---
+++ src/topic_00_define/basic_trms.kt
@@ -0,0 +1,11 @@
+package topic_00_define
+
+/*
+Program : A program is a sequence of instructions (called statements),
+          which are executed one after another in a predictable manner.
+          Sequential flow is the most common and straightforward situation
+          when statements are executed in the order they are written, i.e.,
+          from top to bottom, one after another;
+
+Statement : A statement (or a programming statement) is a single command to be executed
+ */
